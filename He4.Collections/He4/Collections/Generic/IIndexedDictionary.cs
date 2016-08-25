cea0f1649614ad769252d8df3543f4f292d47de1
Justin LaPolla <j.a.lapolla@gmail.com>
2016-08-24 17:31:26 -0700

    IIndexedDictionary
    - We're gonna try adding external documentation to this.

---
+++ He4.Collections/He4/Collections/Generic/IIndexedDictionary.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace He4.Collections.Generic
+{
+
+  /// <summary>
+  /// Does something awesome
+  /// </summary>
+  public interface IIndexedDictionary<TKey, TValue> :
+    System.Collections.Generic.IDictionary<TKey, TValue>,
+    System.Collections.Generic.IList<System.Collections.Generic.KeyValuePair<TKey, TValue>>
+  {
+  }
+}
+
