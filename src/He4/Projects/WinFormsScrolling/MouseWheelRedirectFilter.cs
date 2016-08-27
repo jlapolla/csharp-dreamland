e41b7f640dcc208e262020624c03f7148efbdcf2
Justin LaPolla <j.a.lapolla@gmail.com>
2016-08-27 12:25:16 -0700

    Working on WinFormScrolling classes
    - We're packing multiple classes into the same source file. Maybe we'll
      split it up. Then again, this project is a demonstration, and people
      like to have single source files that they can copy and paste. Not
      sure what to do.

--- src/He4/Projects/WinFormsScrolling/MouseWheelRedirectFilter.cs
+++ src/He4/Projects/WinFormsScrolling/MouseWheelRedirectFilter.cs
@@ -40,7 +40,7 @@ namespace He4.Projects.WinFormsScrolling
   /// Application.Run(Form).
   /// </para>
   /// </remarks>
-  public class MouseWheelRedirectFilter : IMessageFilter
+  internal class MouseWheelRedirectFilter : IMessageFilter
   {
 
     public bool PreFilterMessage(ref Message message)
@@ -48,72 +48,135 @@ namespace He4.Projects.WinFormsScrolling
 
       return false;
     }
+  }
 
-    private abstract class Win32Api
+  internal static class Win32Api
+  {
+  }
+
+  internal class MouseWheelMessageTranslator
+  {
+
+    public bool CanTranslate(Message message)
     {
+
+      bool result;
+
+      switch ((Win32MouseMessages)message.Msg)
+      {
+
+        case Win32MouseMessages.WM_MOUSEWHEEL:
+          result = true;
+          break;
+
+        case Win32MouseMessages.WM_MOUSEHWHEEL:
+          result = true;
+          break;
+
+        default:
+          result = false;
+          break;
+      }
+
+      return result;
     }
 
-    /// <summary>
-    /// Windows mouse input messages.
-    /// </summary>
-    ///
-    /// <remarks>
-    /// <para>
-    /// This enumeration only includes mouse input messages. For details on
-    /// each message, refer to
-    /// https://msdn.microsoft.com/en-us/library/ff468877.aspx.
-    /// </para>
-    ///
-    /// <para>
-    /// For a full list of Windows message types, refer to
-    /// https://msdn.microsoft.com/en-us/library/ms644927.aspx (see the table
-    /// of system defined messages in the Message Types section).
-    /// </para>
-    /// </remarks>
-    private enum Win32MouseMessages : int {
+    public MouseEventArgs Translate(Message message)
+    {
 
-      // Mouse wheel messages
-      WM_MOUSEWHEEL      = 0x020A, // Sent to the focus window when the mouse wheel is rotated.
-      WM_MOUSEHWHEEL     = 0x020E, // Sent to the active window when the mouse's horizontal scroll wheel is tilted or rotated.
+      return null;
+    }
+  }
 
-      // Client area mouse messages
-      WM_MOUSEMOVE       = 0x0200, // Posted to a window when the cursor moves.
-      WM_MOUSEHOVER      = 0x02A1, // Posted to a window when the cursor hovers.
-      WM_MOUSELEAVE      = 0x02A3, // Posted to a window when the cursor leaves.
-      WM_LBUTTONDOWN     = 0x0201, // Posted when the user presses the left mouse button.
-      WM_RBUTTONDOWN     = 0x0204, // Posted when the user presses the right mouse button.
-      WM_MBUTTONDOWN     = 0x0207, // Posted when the user presses the middle mouse button.
-      WM_XBUTTONDOWN     = 0x020B, // Posted when the user presses the first or second X button.
-      WM_LBUTTONUP       = 0x0202, // Posted when the user releases the left mouse button.
-      WM_RBUTTONUP       = 0x0205, // Posted when the user releases the right mouse button.
-      WM_MBUTTONUP       = 0x0208, // Posted when the user releases the middle mouse button.
-      WM_XBUTTONUP       = 0x020C, // Posted when the user releases the first or second X button.
-      WM_LBUTTONDBLCLK   = 0x0203, // Posted when the user double-clicks the left mouse button.
-      WM_RBUTTONDBLCLK   = 0x0206, // Posted when the user double-clicks the right mouse button.
-      WM_MBUTTONDBLCLK   = 0x0209, // Posted when the user double-clicks the middle mouse button.
-      WM_XBUTTONDBLCLK   = 0x020D, // Posted when the user double-clicks the first or second X button.
+  /// <summary>
+  /// Windows mouse input messages.
+  /// </summary>
+  ///
+  /// <remarks>
+  /// <para>
+  /// This enumeration only includes mouse input messages. For details on
+  /// each message, refer to
+  /// https://msdn.microsoft.com/en-us/library/ff468877.aspx.
+  /// </para>
+  ///
+  /// <para>
+  /// For a full list of Windows message types, refer to
+  /// https://msdn.microsoft.com/en-us/library/ms644927.aspx (see the table
+  /// of system defined messages in the Message Types section).
+  /// </para>
+  /// </remarks>
+  internal enum Win32MouseMessages
+  {
 
-      // Nonclient area mouse messages
-      WM_NCMOUSEMOVE     = 0x00A0, // Posted to a window when the cursor is moved within the nonclient area of the window.
-      WM_NCMOUSEHOVER    = 0x02A0, // Posted to a window when the cursor hovers over the nonclient area of the window.
-      WM_NCMOUSELEAVE    = 0x02A2, // Posted to a window when the cursor leaves the nonclient area of the window.
-      WM_NCLBUTTONDOWN   = 0x00A1, // Posted when the user presses the left mouse button while the cursor is within the nonclient area of a window.
-      WM_NCRBUTTONDOWN   = 0x00A4, // Posted when the user presses the right mouse button while the cursor is within the nonclient area of a window.
-      WM_NCMBUTTONDOWN   = 0x00A7, // Posted when the user presses the middle mouse button while the cursor is within the nonclient area of a window.
-      WM_NCXBUTTONDOWN   = 0x00AB, // Posted when the user presses the first or second X button while the cursor is in the nonclient area of a window.
-      WM_NCLBUTTONUP     = 0x00A2, // Posted when the user releases the left mouse button while the cursor is within the nonclient area of a window.
-      WM_NCRBUTTONUP     = 0x00A5, // Posted when the user releases the right mouse button while the cursor is within the nonclient area of a window.
-      WM_NCMBUTTONUP     = 0x00A8, // Posted when the user releases the middle mouse button while the cursor is within the nonclient area of a window.
-      WM_NCXBUTTONUP     = 0x00AC, // Posted when the user releases the first or second X button while the cursor is in the nonclient area of a window.
-      WM_NCLBUTTONDBLCLK = 0x00A3, // Posted when the user double-clicks the left mouse button while the cursor is within the nonclient area of a window.
-      WM_NCRBUTTONDBLCLK = 0x00A6, // Posted when the user double-clicks the right mouse button while the cursor is within the nonclient area of a window.
-      WM_NCMBUTTONDBLCLK = 0x00A9, // Posted when the user double-clicks the middle mouse button while the cursor is within the nonclient area of a window.
-      WM_NCXBUTTONDBLCLK = 0x00AD, // Posted when the user double-clicks the first or second X button while the cursor is in the nonclient area of a window.
+    // Mouse wheel messages
+    WM_MOUSEWHEEL      = 0x020A, // Sent to the focus window when the mouse wheel is rotated.
+    WM_MOUSEHWHEEL     = 0x020E, // Sent to the active window when the mouse's horizontal scroll wheel is tilted or rotated.
 
-      // Miscellaneous mouse messages
-      WM_NCHITTEST       = 0x0084, // Sent to a window in order to determine what part of the window corresponds to a particular screen coordinate.
-      WM_MOUSEACTIVATE   = 0x0021, // Sent when the cursor is in an inactive window and the user presses a mouse button.
-      WM_CAPTURECHANGED  = 0x0215, // Sent to the window that is losing the mouse capture.
+    // Client area mouse messages
+    WM_MOUSEMOVE       = 0x0200, // Posted to a window when the cursor moves.
+    WM_MOUSEHOVER      = 0x02A1, // Posted to a window when the cursor hovers.
+    WM_MOUSELEAVE      = 0x02A3, // Posted to a window when the cursor leaves.
+    WM_LBUTTONDOWN     = 0x0201, // Posted when the user presses the left mouse button.
+    WM_RBUTTONDOWN     = 0x0204, // Posted when the user presses the right mouse button.
+    WM_MBUTTONDOWN     = 0x0207, // Posted when the user presses the middle mouse button.
+    WM_XBUTTONDOWN     = 0x020B, // Posted when the user presses the first or second X button.
+    WM_LBUTTONUP       = 0x0202, // Posted when the user releases the left mouse button.
+    WM_RBUTTONUP       = 0x0205, // Posted when the user releases the right mouse button.
+    WM_MBUTTONUP       = 0x0208, // Posted when the user releases the middle mouse button.
+    WM_XBUTTONUP       = 0x020C, // Posted when the user releases the first or second X button.
+    WM_LBUTTONDBLCLK   = 0x0203, // Posted when the user double-clicks the left mouse button.
+    WM_RBUTTONDBLCLK   = 0x0206, // Posted when the user double-clicks the right mouse button.
+    WM_MBUTTONDBLCLK   = 0x0209, // Posted when the user double-clicks the middle mouse button.
+    WM_XBUTTONDBLCLK   = 0x020D, // Posted when the user double-clicks the first or second X button.
+
+    // Nonclient area mouse messages
+    WM_NCMOUSEMOVE     = 0x00A0, // Posted to a window when the cursor is moved within the nonclient area of the window.
+    WM_NCMOUSEHOVER    = 0x02A0, // Posted to a window when the cursor hovers over the nonclient area of the window.
+    WM_NCMOUSELEAVE    = 0x02A2, // Posted to a window when the cursor leaves the nonclient area of the window.
+    WM_NCLBUTTONDOWN   = 0x00A1, // Posted when the user presses the left mouse button while the cursor is within the nonclient area of a window.
+    WM_NCRBUTTONDOWN   = 0x00A4, // Posted when the user presses the right mouse button while the cursor is within the nonclient area of a window.
+    WM_NCMBUTTONDOWN   = 0x00A7, // Posted when the user presses the middle mouse button while the cursor is within the nonclient area of a window.
+    WM_NCXBUTTONDOWN   = 0x00AB, // Posted when the user presses the first or second X button while the cursor is in the nonclient area of a window.
+    WM_NCLBUTTONUP     = 0x00A2, // Posted when the user releases the left mouse button while the cursor is within the nonclient area of a window.
+    WM_NCRBUTTONUP     = 0x00A5, // Posted when the user releases the right mouse button while the cursor is within the nonclient area of a window.
+    WM_NCMBUTTONUP     = 0x00A8, // Posted when the user releases the middle mouse button while the cursor is within the nonclient area of a window.
+    WM_NCXBUTTONUP     = 0x00AC, // Posted when the user releases the first or second X button while the cursor is in the nonclient area of a window.
+    WM_NCLBUTTONDBLCLK = 0x00A3, // Posted when the user double-clicks the left mouse button while the cursor is within the nonclient area of a window.
+    WM_NCRBUTTONDBLCLK = 0x00A6, // Posted when the user double-clicks the right mouse button while the cursor is within the nonclient area of a window.
+    WM_NCMBUTTONDBLCLK = 0x00A9, // Posted when the user double-clicks the middle mouse button while the cursor is within the nonclient area of a window.
+    WM_NCXBUTTONDBLCLK = 0x00AD, // Posted when the user double-clicks the first or second X button while the cursor is in the nonclient area of a window.
+
+    // Miscellaneous mouse messages
+    WM_NCHITTEST       = 0x0084, // Sent to a window in order to determine what part of the window corresponds to a particular screen coordinate.
+    WM_MOUSEACTIVATE   = 0x0021, // Sent when the cursor is in an inactive window and the user presses a mouse button.
+    WM_CAPTURECHANGED  = 0x0215, // Sent to the window that is losing the mouse capture.
+  }
+
+  internal enum MouseWheelOrientations
+  {
+    Vertical,
+    Horizontal
+  }
+
+  internal class MouseWheelEventArgs : MouseEventArgs
+  {
+
+    private readonly MouseWheelOrientations _WheelOrientation;
+    public MouseWheelOrientations WheelOrientation
+    {
+
+      get
+      {
+
+        return _WheelOrientation;
+      }
+    }
+
+    public MouseWheelEventArgs(MouseButtons button, int clicks, int x, int y, int delta, MouseWheelOrientations wheelOrientation)
+      : base(button, clicks, x, y, delta)
+    {
+
+      _WheelOrientation = wheelOrientation;
     }
   }
 }
