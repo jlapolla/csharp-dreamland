cf1c8a89ccbf751be9f47198f542e2316368b5ca
Justin LaPolla <j.a.lapolla@gmail.com>
2016-08-28 12:48:23 -0700

    Add note about KeyedCollection<TKey, TItem>
    - It's almost an IndexedDictionary, but not quite. The need for
      IndexedDictionary still exists.

--- src/He4/Collections/Generic/IIndexedDictionary.cs
+++ src/He4/Collections/Generic/IIndexedDictionary.cs
@@ -6,6 +6,15 @@ namespace He4.Collections.Generic
   /// <summary>
   /// Does something awesome
   /// </summary>
+  ///
+  /// <remarks>
+  /// <para>
+  /// This is similar to a
+  /// System.Collections.ObjectModel.KeyedCollection&lt;TKey, TItem&gt;, but it
+  /// requires unique keys, and it allows looking up a numerical index from a
+  /// key in O(1).
+  /// </para>
+  /// </remarks>
   public interface IIndexedDictionary<TKey, TValue> :
     System.Collections.Generic.IDictionary<TKey, TValue>,
     System.Collections.Generic.IList<System.Collections.Generic.KeyValuePair<TKey, TValue>>
