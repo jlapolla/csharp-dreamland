262bee5769cdc610f8546deb798fb76437286bc4
Justin LaPolla <j.a.lapolla@gmail.com>
2016-08-28 19:38:15 -0700

    Improve documentation for Win32Utilities
    - Improved XML documentation for the CopyLeastSignificantBytes function.

--- src/He4/Projects/WinFormsScrolling/MouseWheelRedirectFilter.cs
+++ src/He4/Projects/WinFormsScrolling/MouseWheelRedirectFilter.cs
@@ -243,6 +243,9 @@ namespace He4.Projects.WinFormsScrolling
     }
   }
 
+  /// <summary>
+  /// Utilities used by other Win32 classes.
+  /// </summary>
   public static class Win32Utilities
   {
 
@@ -254,25 +257,43 @@ namespace He4.Projects.WinFormsScrolling
     ///
     /// <remarks>
     /// <para>
+    /// The copy operation always proceeds in the direction from the least
+    /// significant byte in the source array to the most significant byte in
+    /// the source array. The startByte argument specifies which byte in the
+    /// source array to start copying at (a positive startByte is similar to
+    /// right shifting the source array before copying). A negative startByte
+    /// begins the copy operation at a byte index before the least significant
+    /// byte in the source array (similar to left shifting the source array
+    /// before copying). Any bytes read outside of the byte range of the source
+    /// array are read as 0x00. The copy operation proceeds until all bytes in
+    /// the destination array have been written to.
+    /// </para>
+    ///
+    /// <para>
     /// When copying bytes, this function takes into account the endianness of
-    /// the current computer architecture. It is designed to be used directly
-    /// with the arrays that are output by (and consumed by)
-    /// System.BitConverter, without having to use Array.Reverse to account for
-    /// endianness.
+    /// the current computer architecture. The function determines which array
+    /// index is the least significant byte in the source array by querying the
+    /// System.BitConverter.IsLittleEndian property. The order of bytes in the
+    /// source array must reflect the endianness of the current computer
+    /// architecture. After the copy operation, the order of bytes in the
+    /// destination array also reflect the endianness of the current computer
+    /// architecture.
     /// </para>
     ///
     /// <para>
-    /// 'startByte' is the zero-based index of the least significant byte to
-    /// copy from 'source'. A 'startByte' of 0 means "Copy bytes from source to
-    /// destination, starting with the least significant byte of source." If
-    /// the function runs out of bytes in 'source' before it has filled all
-    /// indices in 'destination', it fills remaining indicies with 0.
+    /// This function is designed to be used directly with the arrays that are
+    /// output by (and consumed by) System.BitConverter, without having to use
+    /// worry about endianness.
     /// </para>
     ///
     /// <para>
-    /// 'startByte' may be negative. A 'startByte' of -2 means "Put 0 in the
-    /// first two indices of destination, then copy bytes from source to
-    /// destination, starting with the least significant byte of source."
+    /// Using this function in conjunction with System.Bitconverter is similar
+    /// in effect to using casting and bitwise shift operators to select
+    /// certain bytes from an integral type.
+    /// </para>
+    ///
+    /// <para>
+    /// The source array and the destination array must both be zero indexed.
     /// </para>
     /// </remarks>
     public static void CopyLeastSignificantBytes(byte[] source, byte[] destination, int startByte = 0)
