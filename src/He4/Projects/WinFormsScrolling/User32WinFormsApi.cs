93eb85ddf8027eea4af3ee27616eda33bf057d04
Justin LaPolla <j.a.lapolla@gmail.com>
2016-08-31 17:26:21 -0700

    Finish and document User32WinFormsApi
    - Added ControlFromPoint function.
    - Added documentation.

--- src/He4/Projects/WinFormsScrolling/User32WinFormsApi.cs
+++ src/He4/Projects/WinFormsScrolling/User32WinFormsApi.cs
@@ -6,29 +6,67 @@ namespace He4.Projects.WinFormsScrolling
 {
 
   /// <summary>
-  /// Wraps functions in User32Api for use with System.Windows.Forms.
+  /// Provides access to User32Api for Windows Forms applications.
   /// </summary>
+  ///
+  /// <remarks>
+  /// Use this class to access functions in User32Api from a Windows Forms
+  /// application. Direct use of User32Api is discouraged.
+  /// </remarks>
   public static class User32WinFormsApi
   {
 
+    /// <summary>
+    /// Synchronously sends the specified message to a window or windows.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// See also: User32Api.SendMessage(IntPtr, uint, IntPtr, IntPtr).
+    /// </remarks>
     public static void SendMessage(ref Message message)
     {
 
-      IntPtr hWnd = message.HWnd;
       uint msg = unchecked((uint) message.Msg);
 
-      Control control = Control.FromChildHandle(hWnd);
+      Control control = Control.FromChildHandle(message.HWnd);
       if (control == null)
       {
 
-        message.Result = User32Api.SendMessage(hWnd, msg, message.WParam, message.LParam);
+        message.Result = User32Api.SendMessage(message.HWnd, msg, message.WParam, message.LParam);
       }
       else
       {
 
-        HandleRef hRef = new HandleRef(control, hWnd);
+        HandleRef hRef = new HandleRef(control, message.HWnd);
         message.Result = User32Api.SendMessage(hRef, msg, message.WParam, message.LParam);
       }
+    }
+
+    /// <summary>
+    /// Retrieves the control that contains the specified point.
+    /// </summary>
+    ///
+    /// <remarks>
+    /// <para>
+    /// Returns null if no control exists at the given point, or if the point
+    /// is within a hidden or disabled control.
+    /// </para>
+    ///
+    /// <para>
+    /// </para>
+    ///
+    /// <para>
+    /// The point is given in screen coordinates.
+    /// </para>
+    ///
+    /// <para>
+    /// See also: User32Api.WindowFromPoint(Point).
+    /// </para>
+    /// </remarks>
+    public static Control ControlFromPoint(System.Drawing.Point point)
+    {
+
+      return Control.FromChildHandle(User32Api.WindowFromPoint(point));
     }
   }
 }
