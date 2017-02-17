2241cf575507bff7f7bf1b81b50bf92a6546e896
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-17 14:44:07 -0600

    Add basic tests for subclasses

---
+++ src/He4/Reflection/Tests/WritableFieldAccessorTest.cs
@@ -0,0 +1,146 @@
+#if NUNIT
+using NUnit.Framework;
+#else
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+#endif
+
+using System;
+
+namespace He4.Reflection.Tests
+{
+
+#if NUNIT
+  [TestFixture]
+#else
+  [TestClass]
+#endif
+  public class WritableFieldAccessorTest
+  {
+
+    /* String tests */
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenFieldDoesNotExist()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        WritableFieldAccessor<SampleClass, ValueType>.Make("NonExistentMember");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("\"NonExistentMember\" must be a public, non-static field of He4.Reflection.Tests.SampleClass which is assignable from System.ValueType.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenFieldIsNotPublic()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        WritableFieldAccessor<SampleClass, ValueType>.Make("PrivateValue");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("\"PrivateValue\" must be a public, non-static field of He4.Reflection.Tests.SampleClass which is assignable from System.ValueType.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenFieldIsStatic()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        WritableFieldAccessor<SampleClass, ValueType>.Make("StaticValue");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("\"StaticValue\" must be a public, non-static field of He4.Reflection.Tests.SampleClass which is assignable from System.ValueType.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenMemberIsNotAField()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        WritableFieldAccessor<SampleClass, ValueType>.Make("ValueProperty");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("\"ValueProperty\" must be a public, non-static field of He4.Reflection.Tests.SampleClass which is assignable from System.ValueType.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+    /* Duplication tests */
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeDuplicatesTemplateBinding()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = WritableFieldAccessor<SampleClass, ValueType>.Make(
+          WritableFieldAccessor<SampleClass, ValueType>.Make("Value"));
+      accessor.Target = target;
+
+      accessor.Member = 0;
+      Assert.AreEqual(0, target.Value);
+
+      accessor.SetMember(1);
+      Assert.AreEqual(1, target.Value);
+
+      accessor.Member = 2;
+      Assert.AreEqual(2, target.Value);
+    }
+  }
+}
+
