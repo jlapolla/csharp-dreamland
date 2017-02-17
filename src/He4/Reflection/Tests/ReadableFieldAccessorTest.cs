537d0caf2b383ccd8204ab55ff9d971abfb202a7
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-17 11:54:58 -0600

    Move tests to ReadableFieldAccessorTest

--- src/He4/Reflection/Tests/ReadableFieldAccessorTest.cs
+++ src/He4/Reflection/Tests/ReadableFieldAccessorTest.cs
@@ -118,6 +118,78 @@ namespace He4.Reflection.Tests
       Assert.IsTrue(throwsError);
     }
 
+    /* Field tests */
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenFieldDoesNotConform()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        ReadableFieldAccessor<SampleClass, string>.Make("Value");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("He4.Reflection.Tests.SampleClass.Value must conform to System.String.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithPublicField()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableFieldAccessor<SampleClass, ValueType>.Make("Value");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithCovarianceInFields()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableFieldAccessor<SampleClass, object>.Make("Value");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual((object) 0, accessor.Member);
+      Assert.AreEqual((object) 0, accessor.GetMember());
+      Assert.AreNotEqual((object) 1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual((object) 1, accessor.Member);
+      Assert.AreEqual((object) 1, accessor.GetMember());
+      Assert.AreNotEqual((object) 0, accessor.GetMember());
+    }
+
     /* Duplication tests */
 #if NUNIT
     [Test]
