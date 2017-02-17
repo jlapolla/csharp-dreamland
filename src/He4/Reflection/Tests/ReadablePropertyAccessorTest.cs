738fddbce60e1e1a06cc74940b50dbe7dadf4544
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-17 11:28:35 -0600

    Move tests to ReadablePropertyAccessorTest

--- src/He4/Reflection/Tests/ReadablePropertyAccessorTest.cs
+++ src/He4/Reflection/Tests/ReadablePropertyAccessorTest.cs
@@ -118,6 +118,103 @@ namespace He4.Reflection.Tests
       Assert.IsTrue(throwsError);
     }
 
+    /* Property tests */
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenPropertyDoesNotConform()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        ReadablePropertyAccessor<SampleClass, string>.Make("ValueProperty");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("He4.Reflection.Tests.SampleClass.ValueProperty must conform to System.String.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeThrowsWhenPropertyDoesNotHavePublicGetAccessor()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        ReadablePropertyAccessor<SampleClass, ValueType>.Make("ValuePropertySetOnly");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("He4.Reflection.Tests.SampleClass.ValuePropertySetOnly must have a public get accessor.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithReadablePublicProperty()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadablePropertyAccessor<SampleClass, ValueType>.Make("ValuePropertyGetOnly");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithCovarianceInProperties()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadablePropertyAccessor<SampleClass, object>.Make("ValueProperty");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual((object) 0, accessor.Member);
+      Assert.AreEqual((object) 0, accessor.GetMember());
+      Assert.AreNotEqual((object) 1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual((object) 1, accessor.Member);
+      Assert.AreEqual((object) 1, accessor.GetMember());
+      Assert.AreNotEqual((object) 0, accessor.GetMember());
+    }
+
     /* Duplication tests */
 #if NUNIT
     [Test]
@@ -135,10 +232,12 @@ namespace He4.Reflection.Tests
       target.Value = 0;
       Assert.AreEqual(0, accessor.Member);
       Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
 
       target.Value = 1;
       Assert.AreEqual(1, accessor.Member);
       Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
     }
   }
 }
