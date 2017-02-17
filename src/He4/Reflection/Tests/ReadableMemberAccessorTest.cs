20fb33e19a02ffd6ebf21581775b457161bc7a56
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-17 12:08:25 -0600

    Complete ReadableMemberAccessorTest
    - Tests hit all branches.

--- src/He4/Reflection/Tests/ReadableMemberAccessorTest.cs
+++ src/He4/Reflection/Tests/ReadableMemberAccessorTest.cs
@@ -93,18 +93,161 @@ namespace He4.Reflection.Tests
       Assert.IsTrue(throwsError);
     }
 
-    /* Other tests */
 #if NUNIT
     [Test]
 #else
     [TestMethod]
 #endif
-    public void MakeDuplicatesTemplateBinding()
+    public void MakeThrowsWhenMethodHasArguments()
+    {
+
+      bool throwsError = false;
+
+      try
+      {
+
+        ReadableMemberAccessor<SampleClass, ValueType>.Make("GetValueWithExtraArguments");
+      }
+      catch (Exception ex)
+      {
+
+        throwsError = true;
+        Assert.AreEqual("\"GetValueWithExtraArguments\" must be a public, non-static property, zero-argument method, or field of He4.Reflection.Tests.SampleClass.", ex.Message);
+      }
+
+      Assert.IsTrue(throwsError);
+    }
+
+    /* Subclass tests */
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithReadablePublicProperty()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableMemberAccessor<SampleClass, ValueType>.Make("ValuePropertyGetOnly");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithZeroArgumentMethods()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableMemberAccessor<SampleClass, ValueType>.Make("GetValue");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void WorksWithPublicField()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableMemberAccessor<SampleClass, ValueType>.Make("Value");
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+    /* Duplication tests */
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeDuplicatesTemplatePropertyBinding()
     {
 
       var target = SampleClass.Make();
       var accessor = ReadableMemberAccessor<SampleClass, ValueType>.Make(
           ReadableMemberAccessor<SampleClass, ValueType>.Make("ValueProperty"));
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeDuplicatesTemplateMethodBinding()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableMemberAccessor<SampleClass, ValueType>.Make(
+          ReadableMemberAccessor<SampleClass, ValueType>.Make("GetValue"));
+      accessor.Target = target;
+
+      target.Value = 0;
+      Assert.AreEqual(0, accessor.Member);
+      Assert.AreEqual(0, accessor.GetMember());
+      Assert.AreNotEqual(1, accessor.GetMember());
+
+      target.Value = 1;
+      Assert.AreEqual(1, accessor.Member);
+      Assert.AreEqual(1, accessor.GetMember());
+      Assert.AreNotEqual(0, accessor.GetMember());
+    }
+
+#if NUNIT
+    [Test]
+#else
+    [TestMethod]
+#endif
+    public void MakeDuplicatesTemplateFieldBinding()
+    {
+
+      var target = SampleClass.Make();
+      var accessor = ReadableMemberAccessor<SampleClass, ValueType>.Make(
+          ReadableMemberAccessor<SampleClass, ValueType>.Make("Value"));
       accessor.Target = target;
 
       target.Value = 0;
